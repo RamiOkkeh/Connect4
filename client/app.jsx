fbc777720858d57794a7151e762b1f1ac361368b
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-05 17:14:35 +0200

    progresssssss!!!!

--- client/app.jsx
+++ client/app.jsx
@@ -5,84 +5,87 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            class: 'box',
-            cs1: 0,
-            cs2: 0,
-            cs3: 0,
-            cs4: 0,
-            cs5: 0,
-            cs6: 0,
-            cs7: 0
+            0: 'default'
         }
+        this.click = this.click.bind(this)
+        this.click1 = this.click1.bind(this)
+        this.turn = 1
+
+        this.mat = _.range(42, 0)
     }
     click(e) {
-        console.log(e.target.parentElement, 'hello')
-        var num = 'cs' + e.target.value
-        console.log(num)
+        if (this.turn === 1) {
+            this.turn = 2
+        } else {
+            this.turn = 1
+        }
+    }
+    click1(input1) {
         this.setState({
-
+            [input1]: 'box' + this.turn
         })
     }
 
     render() {
         return (
-            <div className="grid">
-                <div className="c" id="1" onClick={this.click}>
-                    <Square currentState={this.state.cs1} y={1} />
-                    <Square currentState={this.state.cs1} y={2} />
-                    <Square currentState={this.state.cs1} y={3} />
-                    <Square currentState={this.state.cs1} y={4} />
-                    <Square currentState={this.state.cs1} y={5} />
-                    <Square currentState={this.state.cs1} y={6} />
+            <div className="grid" onClick={this.click}>
+                {/* <div className="c" id="1" onClick={this.click}>
+                    <Square turn={this.turn} cs={this.state.cs1} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs1} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs1} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs1} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs1} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs1} y={6} />
                 </div>
                 <div className="c" id="2" onClick={this.click}>
-                    <Square currentState={this.state.cs2} y={1} />
-                    <Square currentState={this.state.cs2} y={2} />
-                    <Square currentState={this.state.cs2} y={3} />
-                    <Square currentState={this.state.cs2} y={4} />
-                    <Square currentState={this.state.cs2} y={5} />
-                    <Square currentState={this.state.cs2} y={6} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs2} y={6} />
                 </div>
                 <div className="c" id="3" onClick={this.click}>
-                    <Square currentState={this.state.cs3} y={1} />
-                    <Square currentState={this.state.cs3} y={2} />
-                    <Square currentState={this.state.cs3} y={3} />
-                    <Square currentState={this.state.cs3} y={4} />
-                    <Square currentState={this.state.cs3} y={5} />
-                    <Square currentState={this.state.cs3} y={6} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs3} y={6} />
                 </div>
                 <div className="c" id="4" onClick={this.click}>
-                    <Square currentState={this.state.cs4} y={1} />
-                    <Square currentState={this.state.cs4} y={2} />
-                    <Square currentState={this.state.cs4} y={3} />
-                    <Square currentState={this.state.cs4} y={4} />
-                    <Square currentState={this.state.cs4} y={5} />
-                    <Square currentState={this.state.cs4} y={6} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs4} y={6} />
                 </div>
                 <div className="c" id="5" onClick={this.click}>
-                    <Square currentState={this.state.cs5} y={1} />
-                    <Square currentState={this.state.cs5} y={2} />
-                    <Square currentState={this.state.cs5} y={3} />
-                    <Square currentState={this.state.cs5} y={4} />
-                    <Square currentState={this.state.cs5} y={5} />
-                    <Square currentState={this.state.cs5} y={6} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs5} y={6} />
                 </div>
                 <div className="c" id="6" onClick={this.click}>
-                    <Square currentState={this.state.cs6} y={1} />
-                    <Square currentState={this.state.cs6} y={2} />
-                    <Square currentState={this.state.cs6} y={3} />
-                    <Square currentState={this.state.cs6} y={4} />
-                    <Square currentState={this.state.cs6} y={5} />
-                    <Square currentState={this.state.cs6} y={6} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs6} y={6} />
                 </div>
                 <div className="c" id="7" onClick={this.click}>
-                    <Square currentState={this.state.cs7} y={1} />
-                    <Square currentState={this.state.cs7} y={2} />
-                    <Square currentState={this.state.cs7} y={3} />
-                    <Square currentState={this.state.cs7} y={4} />
-                    <Square currentState={this.state.cs7} y={5} />
-                    <Square currentState={this.state.cs7} y={6} />
-                </div>
+                    <Square turn={this.turn} cs={this.state.cs7} y={1} />
+                    <Square turn={this.turn} cs={this.state.cs7} y={2} />
+                    <Square turn={this.turn} cs={this.state.cs7} y={3} />
+                    <Square turn={this.turn} cs={this.state.cs7} y={4} />
+                    <Square turn={this.turn} cs={this.state.cs7} y={5} />
+                    <Square turn={this.turn} cs={this.state.cs7} y={6} />
+                </div> */}
+                {this.mat.map((elem, i) => <Square key={elem} key1={elem} state={this.state} click1={this.click1} turn={this.turn} />)}
             </div>
         )
     }
--- client/components/Square.jsx
+++ client/components/Square.jsx
@@ -3,12 +3,27 @@ import React from 'react'
 class Square extends React.Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            0: 'default'
+        }
+        this.click = this.click.bind(this)
     }
-
+    click() {
+        var key = this.props.key1 % 7
+        var recurtion = () => {
+            if (this.props.state[key]) {
+                key += 7
+                recurtion()
+            }
+        }
+        console.log(key)
+        recurtion()
+        this.props.click1(key)
+    }
     render() {
         return (
-            <div id="de" className={this.props.y <= this.props.cs1 ? 'colorBox' : 'box'}>
+            <div className="box" onClick={this.click}>
+                <div className={this.props.state[this.props.key1] ? this.props.state[this.props.key1] : 'box'}></div>
             </div>
         )
     }
