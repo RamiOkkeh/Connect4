d180c10114831b20d2a189d4bc0fca5bb1f0ae91
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-05 11:19:55 +0200

    By works, i meant 'doesn't work'

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
+const express = require('express')
 
 
+let app = express();
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let port = 3002;
+
+app.listen(port, () => {
+    console.log('Listening on port: ' + port)
+})
